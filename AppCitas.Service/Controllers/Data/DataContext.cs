e88241b735766a73a04fe4f971cbc76d2a27e261
Valemst <valeriasoto2205@gmail.com>
2022-09-06 08:14:15 -0500

    2

---
+++ AppCitas.Service/Controllers/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using AppCitas.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Service.Controllers.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
